9d073fc22795f616ee1eb4d1caab3f9f2fa8e495
Hkm172Cse <azizulhakim000111@gmail.com>
2022-02-02 18:59:30 +0600

    countup setting successfully

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -1,5 +1,10 @@
 import React, {Component, Fragment} from 'react';
 import {Card,Container,Row,Col,Button} from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faCheckCircle} from '@fortawesome/free-solid-svg-icons';
+import CountUp from 'react-countup';
+import VisibilitySensor from 'react-visibility-sensor';
+
 class Summary extends Component{
     
 
@@ -13,12 +18,30 @@ class Summary extends Component{
                              <Col lg={8} className="countNum">
                                  <Row>
                                  <Col md={6} sm={12} className="text-center">
-                                    <h1 className="text-white">100</h1>
+                                    <h1 className="text-white">
+                                    <CountUp start={0} end={100}>
+                                        {({ countUpRef, start }) => (
+                                            <VisibilitySensor onChange={start} delayeCall>
+                                                <span ref={countUpRef} />
+                                            </VisibilitySensor>
+                                        )}
+                                    </CountUp>
+                                     
+                                    </h1>
                                     <h4 className="text-white">Total Project</h4>
                                     <hr className="bg-white w-25" />
                                 </Col>
                                 <Col md={6} sm={12} className="text-center">
-                                <h1 className="text-white">100</h1>
+                                <h1 className="text-white">
+                                    <CountUp start={0} end={100}>
+                                        {({ countUpRef, start }) => (
+                                            <VisibilitySensor onChange={start} delayeCall>
+                                                <span ref={countUpRef} />
+                                            </VisibilitySensor>
+                                        )}
+                                    </CountUp>
+                                     
+                                    </h1>
                                     <h4 className="text-white">Total Project</h4>
                                     <hr className="bg-white w-25" />
                                 </Col>
@@ -26,14 +49,15 @@ class Summary extends Component{
                                 
                              </Col>
                              
-                             <Col lg={4} className="sumarryCard text-center">
+                             <Col lg={4} md={12} sm={12} className="sumarryCard text-center">
                                 <Card style={{ width: '18rem' }}>
+                                <Card.Title>How I Work</Card.Title>
                                     
-                                    <Card.Body>
-                                    <Card.Title>How I Work</Card.Title>
-                                        <p>SoftWare Requirment</p>
-                                        <p>Software Analysis</p>
-                                        <p>Web Development</p>
+                                    <Card.Body className="summaryLastCard">
+                                    
+                                        <p><FontAwesomeIcon icon={faCheckCircle} /> SoftWare Requirment</p>
+                                        <p><FontAwesomeIcon icon={faCheckCircle} /> Software Analysis</p>
+                                        <p><FontAwesomeIcon icon={faCheckCircle} /> Web Development</p>
                                     </Card.Body>
                                 </Card>
                              </Col>
